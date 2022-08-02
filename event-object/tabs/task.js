a7848e8c19a2e105f04f06d257b28a3decec81d1
prejudicejack <SavinaVG@yandex.ru>
2022-08-02 21:11:18 +0700

    updated task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,20 +1,12 @@
 const tabItem = Array.from(document.getElementsByClassName("tab"));
 const contentTab = Array.from(document.getElementsByClassName("tab__content"));
 
-for(i = 0;i < tabItem.length; i++) {
-	let tab = tabItem[i];
-	let tabItemIndex = tabItem.indexOf(tab);
+tabItem.forEach(element => element.addEventListener('click', onClick));
 
-	tab.addEventListener('click', onClick);
-
-	function onClick() {
-		const activeTab = document.querySelector('.tab_active');
-		const activeTabContent = document.querySelector('.tab__content_active');
-
-		activeTab.className = "tab";
-		activeTabContent.className = "tab__content";
-
-		tab.className += " tab_active";
-		contentTab[tabItemIndex].className += " tab__content_active"
-	}
+function onClick(event) {
+	tabItem.forEach(element => element.className = 'tab');
+	event.target.classList.add('tab_active');
+	contentTab.forEach(element => element.className = 'tab__content');
+	const tabItemIndex = tabItem.indexOf(event.target);
+	contentTab[tabItemIndex].classList.add('tab__content_active');
 }
